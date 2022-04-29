4a64237ae87c71866c387b7e2b6200b7774261b1
LightBlueGamer <timjonsson03@hotmail.com>
2022-04-29 12:10:17 +0200

    Added more things

--- src/main/java/net/thelightbluegame/lithereal/block/custom/ModStoneButtonBlock.java
+++
@@ -1,9 +0,0 @@
-package net.thelightbluegame.lithereal.block.custom;
-
-import net.minecraft.block.StoneButtonBlock;
-
-public class ModStoneButtonBlock extends StoneButtonBlock {
-    public ModStoneButtonBlock(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/net/thelightbluegame/lithereal/world/gen/ModWorldGen.java
+++
@@ -1,8 +0,0 @@
-package net.thelightbluegame.lithereal.world.gen;
-
-public class ModWorldGen {
-    public static void generateModWorldGen() {
-        ModOreGeneration.generateOres();
-        ModTreeGeneration.generateTrees();
-    }
-}
